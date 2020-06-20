b2053e856ea08b6074e93cc7287aeedf5fbd96b9
bowinpallyshivaprasad <52326942+bowinpallyshivaprasad@users.noreply.github.com>
2020-06-20 13:32:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+chrome.runtime.onMessage.addListener(
+    function(request) {
+        if(request.addUrl) {
+            chrome.windows.getCurrent({populate:true},(window) => {
+                const urls = window.tabs.filter(t => t.url.indexOf("chrome://") != 0);
+
+                chrome.storage.sync.get(["urlLists"], (obj) => {
+                    const currentLists = obj.urlLists ? obj.urlLists : [];
+                    const mergedLists = [...currentLists, ...[{title: request.title, urls: urls}]];
+                    console.log(mergedLists);
+                    chrome.storage.sync.set({urlLists: mergedLists})
+                });
+            });
+
+        }
+    }
+);
